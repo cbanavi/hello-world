2a9c3ae2ab6c0113845d2fa8c65b4e8a53d91e01
cbanavi <cbanavi@gmail.com>
2022-07-10 17:40:59 -0700

    Creat new file hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -4,3 +4,4 @@
 >git commit -m "created new file hello-world.py" # commits the files you just added
 >git push origin master # will push the code you committed to the remote server, master branch
 >git log # this will show you a history of your commits
+fj
